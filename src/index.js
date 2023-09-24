5be6af9ea3f15ef729fcd63b8c48a719ee44287e
Akram Khan <akrammiru@gmail.com>
2023-09-25 02:09:14 +0600

    feat: add amplify ui

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
 import {AmplifyProvider} from "@aws-amplify/ui-react";
 import awsconfig from './aws-exports';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <AmplifyProvider>
-      <App />
-    </AmplifyProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AmplifyProvider>
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
+        </AmplifyProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
